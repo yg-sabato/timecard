10b14241bc3b01ba9363494346e87ad14aca2eb2
Sabato <yg-sabato@c-table.co.jp>
2023-12-26 01:00:20 +0900

    timestampのテーブルを作成

---
+++ app/Http/Controllers/TimestampController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Timestamp;
+use Illuminate\Http\Request;
+
+class TimestampController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Timestamp $timestamp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Timestamp $timestamp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Timestamp $timestamp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Timestamp $timestamp)
+    {
+        //
+    }
+}
