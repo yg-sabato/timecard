a266eb27c0a63b4f1f8a8695ab54114e7d392930
Sabato <yg-sabato@c-table.co.jp>
2024-01-08 17:53:17 +0900

    Refactor index.blade.php layout and styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,62 +11,62 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
-    </head>
 
     <header class="bg-dark py-3">
-        <div class="container">
+        <div class="inner">
             <h1 class="text-white">業務打刻ツール</h1>
         </div>
     </header>
 
     <body>
-        <form method="post" action="{{ route('timestamps-submit') }}" class="p-3">
-            @csrf
-            <p id="time"></p>
-
-            <!-- Stamp Type -->
-            <label class="form-label">業務を始めるか、終わるかを選んでください</label>
-            <div class="mb-3">
-                <div class="form-check form-check-inline">
-                    <input required type="radio" id="in" name="stamp_type" value="in" class="form-check-input">
-                    <label for="in" class="form-check-label">開始</label>
+        <div class=inner>
+            <form method="post" action="{{ route('timestamps-submit') }}" class="p-3">
+                @csrf
+                <p id="time"></p>
+                <!-- Stamp Type -->
+                <label class="form-label">業務を始めるか、終わるかを選んでください</label>
+                <div class="mb-3">
+                    <div class="form-check form-check-inline">
+                        <input required type="radio" id="in" name="stamp_type" value="in" class="form-check-input">
+                        <label for="in" class="form-check-label">開始</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input required type="radio" id="out" name="stamp_type" value="out" class="form-check-input">
+                        <label for="out" class="form-check-label">終了</label>
+                    </div>
                 </div>
-                <div class="form-check form-check-inline">
-                    <input required type="radio" id="out" name="stamp_type" value="out" class="form-check-input">
-                    <label for="out" class="form-check-label">終了</label>
+            
+                <!-- Description -->
+                <div class="mb-3">
+                    <label for="description" class="form-label">どんな業務を行うか宣言してください(終了時は登録されないので不要です)</label>
+                    <input type="text" id="description" name="description" class="form-control">
                 </div>
-            </div>
-        
-            <!-- Description -->
-            <div class="mb-3">
-                <label for="description" class="form-label">どんな業務を行うか宣言してください(終了時は登録されないので不要です)</label>
-                <input type="text" id="description" name="description" class="form-control">
-            </div>
-        
-            <!-- Submit Button -->
-            <div class="mb-3">
-                <button type="submit" class="btn btn-primary">打刻</button>
-            </div>
-        </form>
-        
-        <table class="table table-bordered table-hover table-striped">
-            <thead class="table-dark">
-                <tr>
-                    <th>日時</th>
-                    <th>開始終了の種別</th>
-                    <th>業務内容</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($timestamps as $timestamp)
-                <tr>
-                    <td>{{ $timestamp->created_at }}</td>
-                    <td>{{ $timestamp->stamp_type === 'in' ? '開始' : '終了' }}</td>
-                    <td>{{ $timestamp->description }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+            
+                <!-- Submit Button -->
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary">打刻</button>
+                </div>
+            </form>
+            
+            <table class="table table-bordered table-hover table-striped">
+                <thead class="table-dark">
+                    <tr>
+                        <th>日時</th>
+                        <th>開始終了の種別</th>
+                        <th>業務内容</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($timestamps as $timestamp)
+                    <tr>
+                        <td>{{ $timestamp->created_at }}</td>
+                        <td>{{ $timestamp->stamp_type === 'in' ? '開始' : '終了' }}</td>
+                        <td>{{ $timestamp->description }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
         
         
         <script>
@@ -88,6 +88,14 @@
                 setInterval(updateTime, 1000);
             })();
         </script>
+
+        <style>
+            .inner{
+                width: 100%;
+                max-width: 920px;
+                margin: 0 auto;
+            }
+        </style>
     </body>
 
 </html>
